c08ee263d981ff36435de6faa4a91195030d2917
TheWeepingSage <kanabarmillen@gmail.com>
2020-04-04 00:42:28 +0530

    initial commit

---
+++ STADS/Feature_Extraction/tag.m
@@ -0,0 +1,46 @@
+function [arr_sums, arr_merge] = tag(arr_in_img)
+    [length,breadth] = size(arr_in_img);
+    arr_out_img = zeros(length+2, breadth+2, 2);
+    arr_out_img(2:length+1, 2:breadth+1, 1) = arr_in_img;
+    tag_num = 1;
+    merge_num = 1;
+    arr_sums = zeros(20,3);
+    arr_merge = zeros(20, 2);
+    for i_tag = 2:length+1
+        for j_tag = 2:breadth+1
+            if arr_out_img(i_tag, j_tag, 1) == 1
+                left = arr_out_img(i_tag-1, j_tag, 1);
+                above = arr_out_img(i_tag, j_tag-1, 1);
+                if left ==1
+                    if above == 1
+                        if arr_out_img(i_tag-1, j_tag, 2) == arr_out_img(i_tag, j_tag-1, 2)
+                            tag = arr_out_img(i_tag, j_tag-1, 2);
+                            arr_out_img(i_tag, j_tag, 2) = tag;
+                            arr_sums(tag) = arr_sums(tag, :) + [i_tag, j_tag, 1];
+                        else
+                            tag_left = arr_out_img(i_tag-1, j_tag, 2);
+                            tag_above = arr_out_img(i_tag, j_tag-1, 2);
+                            arr_merge(merge_num, :) = [min(tag_left, tag_above), max(tag_left, tag_above)];
+                            merge_num = merge_num +1;
+                            arr_out_img(i_tag, j_tag, 2) = tag_left;
+                            arr_sums(tag_left) = arr_sums(tag_left) + [i_tag, j_tag, 1];
+                        end
+                    else
+                        tag = arr_out_img(i_tag-1, j_tag, 2);
+                        arr_out_img(i_tag, j_tag, 2) = tag;
+                        arr_sums(tag, :) = arr_sums(tag, :) + [i_tag, j_tag, 1];
+                    end
+                else
+                    if above == 1
+                        tag = arr_out_img(i_tag, j_tag+1, 2);
+                        arr_out_img(i_tag, j_tag, 2) = tag;
+                        arr_sums(tag, :) = arr_sums(tag, :) + [i_tag, j_tag, 1];
+                    else
+                        arr_out_img(i_tag, j_tag, 2) = tag_num;
+                        arr_sums(tag_num) = [i_tag, j_tag, 1];
+                        tag_num = tag_num +1;
+                    end
+                end
+            end
+        end
+    end
