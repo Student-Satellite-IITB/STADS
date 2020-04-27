8faf8706c26663527aad580343ce38a415188cd3
Neilabh Banzal <34180039+Neilabh21@users.noreply.github.com>
2020-04-27 11:16:59 +0530

    Remove hygdata_v3.csv, Simulation_HYG.csv, level.mat's, empty.txt's, MATLAB.gitignore, se_load_csv.m; Update main.m, se_main.m, se_preprocessing.m, se_ang_dist.m, se_celestial2lens.m, se_edit_input_data.m, Matlab Graph.svg; Add se_lens2sensor.m, se_SKY2000.csv, se_SKY2000.mat; Rename se_celestial2lens.m to se_icrs2lens.m, se_edit_input_data.m to se_load_input_data.m
    REMOVE -
    
    hygdata_v3.csv - as we have shifted to SKY 2000 Catalogue
    
    Simulation_HYG.csv - Simulation HYG Catalogue - also removed
    
    level.mat (everywhere) - not needed
    
    empty.txt (everywhere) - no longer necessary
    
    MATLAB.gitignore - It doesn't work with Github Desktop - instead, .gitignore has been added
    
    se_load_csv.m - adopted into se_preprocessing.m and se_ang_dist.m
    
    UPDATE -
    
    main.m - Comment added at top
    
    se_main.m - level structure removed, changed order - trimming according to angular distance before icrs to lens transformation calculations, add se_lens2sensor
    
    se_preprocessing.m - level struture removed, catalogue updated, variable names changed accordingly, table stored in a mat file instead of a csv file, a copy saved in csv file only in debugging mode, unnecessary variables cleared in debugging mode, success messages updated
    
    se_ang_dist.m -  level struture removed, , unnecessary variables cleared in debugging mode, success messages updated, functions renamed
    
    se_celestial2lens.m - remove level structure, rename Rotation Matrices, update debug messages
    
    se_edit_input_data.m - remove level structure, remove unnecessary variables, update debug messages
    
    Matlab Graph.svg - Updated to reflect the current repo structure
    
    ADD -
    
    se_lens2sensor.m - converts 3d coordinates in lens frame to 2d coordinates in sensor frame
    
    se_SKY2000.csv - preprocessed catalogue for Sensor Modelling
    
    se_SKY2000.mat - preprocessed catalogue for Sensor Modelling
    
    RENAME -
    
    se_celestial2lens.m to se_icrs2lens.m - as the Celestial Frame is ICRS
    
    se_edit_input_data.m to se_load_input_data.m - as we are not editing the inputs, but loading the input values

--- Sensor_Modelling/se_main.m
+++ Sensor_Modelling/se_main.m
@@ -1,44 +1,31 @@
 % Main Function for Sensor Modelling Block
-%   Detailed explanation goes here
-
-% Add everything to path
-load('level.mat', 'level');
-if (level == 1)
-    addpath(genpath('./Inputs/'));
-    addpath(genpath('./Preprocessing/'));
-    addpath(genpath('./Processing/'));
-elseif (level == 0)
-    addpath(genpath('./Sensor_Modelling/Inputs/'));
-    addpath(genpath('./Sensor_Modelling/Preprocessing/'));
-    addpath(genpath('./Sensor_Modelling/Processing/'));
-else
-    error('Error: level.mat file missing or incorrect path');
-end
+%   The Sensor Model Generates the Image as seen by the Star Tracker in
+%   space. This is input to the Feature Extraction Block.
     
-% Run the edit_input_data.m file.
-se_edit_input_data;
+%% Load Input Data
+se_load_input_data;
 
-% Load se_pp from se_variables.mat
+%% Load se_pp & se_debug_run from se_variables.mat
 load('se_variables.mat', 'se_pp', 'se_debug_run');
 
 % Temporary - to be removed after  debugging
-% if (se_debug_run == 1); se_pp = 1; end
+if (se_debug_run == 1); se_pp = 1; end
 
-% Preprocessing of the HYG Database, if se_pp != 0
+%% Preprocess the SKY 2000 Catalogue
 if (se_pp == 1)
     se_preprocessing; 
 elseif (se_pp == 0)
     disp('Preprocessing: Skipped');
 end
 
-% Load the csv file into mat file
-se_load_csv;
-
-% Convert star coordinates from celestial frame to lens frame
-se_celestial2lens;
-
-% Calculate the angular distance and trim the database to within FOV
+%% Trim the database to within FOV
 se_ang_dist;
 
-%'SE Main: Success';
-disp('Sensor Modelling Main: Success');
+%% ICRS to Lens Frame
+se_icrs2lens;
+
+%% Lens Frame to Sensor Frame
+se_lens2sensor;
+
+% Display Success Message;
+fprintf('Sensor Modelling Main: Success \n \n');
