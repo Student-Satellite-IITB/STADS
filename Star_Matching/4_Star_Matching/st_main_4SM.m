616dbc05889a7db9fa76939f83666530750635ae
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-04-30 17:39:25 +0530

    UPDATE - 4-Star Matching Algorithm Main Files
    ADD :
    st_main_4SM.m
    st_main_4SM_v2.m
    
    UPDATE :
    RM_4_Star_Matching_Algorithm.pdf
    
    DELETE :
    st_main_4_str_mtch.m

--- Star_Matching/4_Star_Matching/st_main_4SM.m
+++ Star_Matching/4_Star_Matching/st_main_4SM.m
@@ -1,22 +1,30 @@
 %% Load Files
 % Load constants
-load('st_constants_4_str_mtch.mat');
+load('.\Star_Matching\4_Star_Matching\Preprocessing\st_constants_4SM.mat');
 
 % Read - Guide Star Catalogue
 st_GD_SC = readmatrix('.\Catalogue\SKY2000\Catalogues\Guide_Star_Catalogue.csv');
 
 % Read - Reference Star Catalogue 
-st_RF_SC = readmatrix('.\Star_Matching\4_Star_Matching\Preprocessing\Reference_Star_Catalogue_4_str_mtch.csv');
+st_RF_SC = readmatrix('.\Star_Matching\4_Star_Matching\Preprocessing\Reference_Star_Catalogue_4SM.csv');
 
 %% Read Input
-load ('st_input.mat'); % Load input file - consists of centroids
+
+% Load input file - consists of centroids
+load ('.\Star_Matching\4_Star_Matching\Input\st_input.mat');
 
 % Generate body-frame vectors of stars 
 st_bi = st_gnrt_bi(fe_output, n_fe_strs, Focal_Length); 
 
 %% 4-Star Matching Alogorithm
+% Run 4-Star Matching Algorithm
+%st_4SM_constants = [n_rw_GC, st_M, st_Q, st_DELTA];
+%[st_SMM, st_mtch_rows] = st_4SM(st_bi(1:4, :), st_RF_SC, st_4SM_constants);
+
+
 % Generate input in the format required by 4-Star Matching Algorithm
-[c_img_AngDst, c_fe_IDs] = st_gnrt_ip_4_str_mtch(st_bi, n_fe_strs); 
+[c_img_AngDst, c_fe_IDs] = st_gnrt_ip_4SM(st_bi); 
+
 
 % Caluculate Star Identification Matrix
 st_SIM = st_gnrt_SIM(c_img_AngDst, st_RF_SC, n_rw_GC, st_M, st_Q, st_DELTA); 
@@ -27,7 +35,7 @@ st_SIM = st_gnrt_SIM(c_img_AngDst, st_RF_SC, n_rw_GC, st_M, st_Q, st_DELTA);
 %% Generate Output
 
 % Generate output in the format required by Estimation
-[st_op_bi, st_op_ri, n_st_strs] = st_gnrt_op_4_str_mtch(st_SMM, st_bi, st_GD_SC); 
+[st_op_bi, st_op_ri, n_st_strs] = st_gnrt_op_4SM(st_SMM, st_bi, st_GD_SC); 
 %% Write Output
 % Write Output of Star Matching
 save('.\Star_Matching\4_Star_Matching\Output\st_output.mat', 'n_st_strs', 'st_mtch_rows',...
