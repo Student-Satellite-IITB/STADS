f28a0427e642763c96d4eabbe6f19658328b0b86
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-04-30 17:37:51 +0530

    UPDATE - 4-Star Matching Algorithm: Preprocessing
    ADD :
    Reference_Star_Catalogue_4SM.csv
    st_constants_4SM.m
    st_RF_SC_4SM.m
    st_constants_4SM.mat
    
    UPDATE :
    RM_4_Star_Matching_Algorithm_Preprocessing.pdf
    
    DELETE :
    Reference_Star_Catalogue_4_str_mtch.csv
    st_constants_4_str_mtch.m
    st_constants_4_str_mtch.m
    st_RF_SC_4_str_mtch.m

--- Star_Matching/4_Star_Matching/Preprocessing/st_RF_SC_4SM.m
+++ Star_Matching/4_Star_Matching/Preprocessing/st_RF_SC_4SM.m
@@ -1,17 +1,17 @@
 %% NOTE:
 % -----
-% This script should be run only by <<< st_constants_4_str_mtch >>> !!!!
+% This script should be run only by <<< st_constants_4SM >>> !!!!
 % The script will throw an error if run by itself.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Read Preprocessed Star Catalogue
 % Read Guide Star Catalogue
-GD_SC = readmatrix('.\Catalogue\SKY2000\Catalogues\Guide_Star_Catalogue.csv');
+st_GD_SC = readmatrix('.\Catalogue\SKY2000\Catalogues\Guide_Star_Catalogue.csv');
 
 % Read - Preprocessed Star Catalogue (which contains the star pairs)
-PP_SC = readmatrix('.\Catalogue\SKY2000\Catalogues\Preprocessed_Star_Catalogue.csv');
+st_PP_SC = readmatrix('.\Catalogue\SKY2000\Catalogues\Preprocessed_Star_Catalogue.csv');
 
 % Extract 'Angular distance - in cos(thetha)' from Preprocessed catalogue
-c_AngDst_cos = PP_SC(:,3); 
+c_AngDst_cos = st_PP_SC(:,3); 
 
 %% Construct K-Vector
 
@@ -19,25 +19,25 @@ c_AngDst_cos = PP_SC(:,3);
 [K_Vec, st_M, st_Q, ~] = st_gnrt_K_Vec(c_AngDst_cos, st_M_EPS, true); 
 
 %% Create Reference Star Catalogue
-RF_SC = [PP_SC(:, 1:2) , K_Vec]; % Append columns to Reference catalogue
+st_RF_SC = [st_PP_SC(:, 1:2) , K_Vec]; % Append columns to Reference Star Catalogue
 
 % Create Table
-RF_SC_table = array2table(RF_SC, 'VariableNames',...
+st_RF_SC_table = array2table(st_RF_SC, 'VariableNames',...
                         {'SSP_ID_1', 'SSP_ID_2', 'K_Vec'});
 
 % Number of star pairs (Number of rows - Reference catalogue)
-sz = size(RF_SC); % Size of REF_CAT
-n_rw_RC = sz(1); 
+sz = size(st_RF_SC); % Size of REF_CAT
+st_n_RC = sz(1); 
 
 % Number of guide stars (Number of rows - Guide catalogue)
-sz = size(GD_SC); % Size of GD_CAT
-n_rw_GC = sz(1); 
+sz = size(st_GD_SC); % Size of GD_CAT
+st_n_GC = sz(1); 
 
 disp('Done: Preprocessing');
 
 if write_csv == 1
     % Create Reference catalogue
-    writetable(RF_SC_table,... 
-    '.\Star_Matching\4_Star_Matching\Preprocessing\Reference_Star_Catalogue_4_str_mtch.csv'); 
+    writetable(st_RF_SC_table,... 
+    '.\Star_Matching\4_Star_Matching\Preprocessing\Reference_Star_Catalogue_4SM.csv'); 
     disp('Done: Write Reference Catalogue');
 end
