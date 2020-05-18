dc818bc1899341dbd0d24f5febb58bc0c9cb5d3e
TheWeepingSage <kanabarmillen@gmail.com>
2020-05-18 16:39:15 +0530

    Changed test_tag_2.m Made changes to test more cases

--- Feature_Extraction/Functions/Test_Feature_Extraction/test_tag_2.m
+++ Feature_Extraction/Functions/Test_Feature_Extraction/test_tag_2.m
@@ -4,11 +4,14 @@ tests = functiontests(localfunctions);
 end
 
 function testSums(testCase)
-    N=9;
+    N=1;
     for n =1:N
-        test_case_1 = load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/test_case_" +n+ "/input.csv");
-        [arr_sums, ~, ~] = tag_2(test_case_1);
-        arr_expected_sums = int32(load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/test_case_" + n +"/sums.csv"));
+        test_case = importdata("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/moderate/moderate_10x100/input.csv");
+        [arr_sums, ~, ~] = tag_2(test_case);
+        arr_expected_sums = int32(load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/moderate/moderate_10x100/sums.csv"));
+        %temp = arr_expected_sums(:, 1);
+        %arr_expected_sums(:, 1) = arr_expected_sums(:, 2);
+        %arr_expected_sums(:, 2) = temp;
         verifyEqual(testCase, arr_sums, arr_expected_sums);
     end
 end
@@ -16,9 +19,9 @@ end
 function testConstants(testCase)
      N=9;
     for n =1:N
-        test_case_1 = load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/test_case_" + n +"/input.csv");
-        [~, num_tags, num_final_tags] = tag_2(test_case_1);
-        arr_expected_constants = load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/test_case_" + n + "/constants.csv");
+        test_case = importdata("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/moderate/moderate_10x100/input.csv");
+        [~, num_tags, num_final_tags] = tag_2(test_case);
+        arr_expected_constants = load("C:/Users/Millen/OneDrive/Documents/SSP Elec/MATLAB Codes/STADS/Feature_Extraction/Functions/Test_Feature_Extraction/moderate/moderate_10x100/constants.csv");
         arr_expected_constants = arr_expected_constants+[1, 1];
         verifyEqual(testCase, [num_tags, num_final_tags], arr_expected_constants);
     end
