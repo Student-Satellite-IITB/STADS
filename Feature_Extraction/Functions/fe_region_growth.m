562cb817fec4129a28b5630c7a5c9c2da12f66b9
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-05-24 10:45:07 +0530

    Update fe_region_growth.m
    UPDATE - fe_region_growth.m separated code into sections and changed input to array

--- Feature_Extraction/Functions/fe_region_growth.m
+++ Feature_Extraction/Functions/fe_region_growth.m
@@ -1,32 +1,36 @@
-function centroids = fe_region_growth(filename)
-    % read the file containing image
-    orig_img = readmatrix(filename);
+function centroids = fe_region_growth(arr_in_img)
+    %% pad and get constants
     % put a padding of zeroes all around
-    img = padarray(orig_img, [1, 1], 0, 'both');
+    arr_out_img = padarray(arr_in_img, [1, 1], 0, 'both');
     % get the x and y sizes respectively for iterating across the image
-    shape = size(img);
-    x_size = shape(1);
-    y_size = shape(2);
+    arr_shape = size(arr_out_img);
+    x_size = arr_shape(1);
+    y_size = arr_shape(2);
     % number of pixels to be skipped
     n = 3;
+    
+    %% initialization to zeroes
     % creating table that will contain the centroid data sums
     x_sum = zeros(50, 1);
     y_sum = zeros(50, 1);
     pixel_sum = zeros(50, 1);
     num_pixels = zeros(50, 1);
     centroid_data = table(x_sum, y_sum, pixel_sum, num_pixels);
-    % keeping track of the star number
-    star_num = 1;
+    
+    %% iterating through image
+    star_num = 1; % counter for star number
     for i = 2 : n : x_size - 1
         for j = 2 : n : y_size-1
-            if img(i, j) > 9 % arbitrary threshold for now
-                [img, centroid_data] = fe_get_data(img, i, j, star_num, centroid_data);
+            if arr_out_img(i, j) > 9 % arbitrary threshold for now
+                [arr_out_img, centroid_data] = fe_get_data(arr_out_img, i, j, star_num, centroid_data);
                 star_num  = star_num + 1;
             end
         end
     end
-
+    
+    %% getting the final required data
     x_cen = centroid_data{1:star_num-1, 'x_sum'}./centroid_data{1:star_num-1, 'pixel_sum'};
     y_cen = centroid_data{1:star_num-1, 'y_sum'}./centroid_data{1:star_num-1, 'pixel_sum'};
     centroids = [x_cen, y_cen];
+    
 end
