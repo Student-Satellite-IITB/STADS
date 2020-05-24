463c7968d269954a15da6d70a4a0dc00bf87a0f9
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-05-24 10:58:27 +0530

    Update fe_get_data.m
    UPDATE - fe_get_data.m changed variable names according to variable nomenclature guidelines

--- Feature_Extraction/Functions/fe_get_data.m
+++ Feature_Extraction/Functions/fe_get_data.m
@@ -1,22 +1,22 @@
-function [img, centroid_data] = fe_get_data(img, i, j, star_num, centroid_data)
+function [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth, j_region_growth, star_num, centroid_data_st)
     %% base case
-    if img(i, j) <= 9 % arbitrary threshold
+    if arr_img(i_region_growth, j_region_growth) <= 9 % arbitrary threshold
         return;
     end
     
     %% updating centroid_data
     % -2 is there so that it matches with the output of python generated
     % images
-    centroid_data{star_num, 'x_sum'} = centroid_data{star_num, 'x_sum'} + img(i, j) * (i - 2);
-    centroid_data{star_num, 'y_sum'} = centroid_data{star_num, 'y_sum'} + img(i, j) * (j - 2);
-    centroid_data{star_num, 'pixel_sum'} = centroid_data{star_num, 'pixel_sum'} + img(i, j);
-    centroid_data{star_num, 'num_pixels'} = centroid_data{star_num, 'num_pixels'} + 1;
+    centroid_data_st{star_num, 'x_sum'} = centroid_data_st{star_num, 'x_sum'} + arr_img(i_region_growth, j_region_growth) * (i_region_growth - 2);
+    centroid_data_st{star_num, 'y_sum'} = centroid_data_st{star_num, 'y_sum'} + arr_img(i_region_growth, j_region_growth) * (j_region_growth - 2);
+    centroid_data_st{star_num, 'pixel_sum'} = centroid_data_st{star_num, 'pixel_sum'} + arr_img(i_region_growth, j_region_growth);
+    centroid_data_st{star_num, 'num_pixels'} = centroid_data_st{star_num, 'num_pixels'} + 1;
     % setting to 0 so that i does not go into infinite loop
-    img(i, j) = 0;
+    arr_img(i_region_growth, j_region_growth) = 0;
     
     %% recursive call to 4 neighbours
-    [img, centroid_data] = fe_get_data(img, i - 1, j, star_num, centroid_data);
-    [img, centroid_data] = fe_get_data(img, i + 1, j, star_num, centroid_data);
-    [img, centroid_data] = fe_get_data(img, i, j - 1, star_num, centroid_data);
-    [img, centroid_data] = fe_get_data(img, i, j + 1, star_num, centroid_data);
+    [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth - 1, j_region_growth, star_num, centroid_data_st);
+    [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth + 1, j_region_growth, star_num, centroid_data_st);
+    [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth, j_region_growth - 1, star_num, centroid_data_st);
+    [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth, j_region_growth + 1, star_num, centroid_data_st);
 end
