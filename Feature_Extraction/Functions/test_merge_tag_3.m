bcfc29bb9a6972327195b9fb80b4d583547373d1
bvcxz1 <62253557+bvcxz1@users.noreply.github.com>
2020-05-25 12:21:59 +0530

    Update test_merge_tag_3.m

--- Feature_Extraction/Functions/test_merge_tag_3.m
+++ Feature_Extraction/Functions/test_merge_tag_3.m
@@ -7,8 +7,8 @@ function testCoords(testCase)
     n=8;
     for I = 0:n
         test_case_1 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/sums.csv");
-        a = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
-        [arr_star_coordinates, ~] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
+        test_case_2 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
+        [arr_star_coordinates, ~] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), test_case_2(1), test_case_2(2));
         arr_expected_coordinates = double(load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/output_coordinates.csv"));
         arr_expected_coordinates2 = arr_expected_coordinates(:,1:2);
         check = abs(arr_expected_coordinates2 - arr_star_coordinates);
@@ -21,8 +21,8 @@ function testConstants(testCase)
      n=8;
     for I = 0:n
         test_case_1 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/sums.csv");
-        a = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
-        [, num_stars] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
+        test_case_2 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
+        [, num_stars] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), test_case_2(1), test_case_2(2));
         expected_stars = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/output_num.csv");
         verifyEqual(testCase, num_stars, expected_stars);
     end
