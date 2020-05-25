2405144e679edea74bae188f7a935a765f8b940a
bvcxz1 <62253557+bvcxz1@users.noreply.github.com>
2020-05-25 12:17:23 +0530

    Update test_merge_tag_3.m

--- Feature_Extraction/Functions/test_merge_tag_3.m
+++ Feature_Extraction/Functions/test_merge_tag_3.m
@@ -1,5 +1,5 @@
-function tests = test_merge_tag_5
-%test_merge_tag_5 test code for merge_tag_5
+function tests = test_merge_tag_3
+%test_merge_tag_3 test code for merge_tag_3
 tests = functiontests(localfunctions);
 end
 
@@ -8,7 +8,7 @@ function testCoords(testCase)
     for I = 0:n
         test_case_1 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/sums.csv");
         a = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
-        [arr_star_coordinates, ~] = merge_tag_6(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
+        [arr_star_coordinates, ~] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
         arr_expected_coordinates = double(load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/output_coordinates.csv"));
         arr_expected_coordinates2 = arr_expected_coordinates(:,1:2);
         check = abs(arr_expected_coordinates2 - arr_star_coordinates);
@@ -22,7 +22,7 @@ function testConstants(testCase)
     for I = 0:n
         test_case_1 = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/sums.csv");
         a = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/constants.csv");
-        [, num_stars] = merge_tag_6(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
+        [, num_stars] = merge_tag_3(test_case_1(1), test_case_1(2), test_case_1(3), test_case_1(4), test_case_1(5), a(1), a(2));
         expected_stars = load("C:/Users/aravi/OneDrive/Documents/MATLAB/test_cases/test_" +I+ "/output_num.csv");
         verifyEqual(testCase, num_stars, expected_stars);
     end
