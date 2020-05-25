5d65dbaf60c58fd619b2b2cd85cc3527e358d867
bvcxz1 <62253557+bvcxz1@users.noreply.github.com>
2020-05-25 12:05:14 +0530

    Update merge_tag_3.cpp

--- Feature_Extraction/Functions/merge_tag_3.cpp
+++ Feature_Extraction/Functions/merge_tag_3.cpp
@@ -4,77 +4,110 @@
 input consisting of
 1. An array of sums of coordinates along x and y
 2. An array of weights
-3. Count of number of pixels in each tagged region
-4. Count of number of final tags
-5. An array of tags corresponding to final tags where the first column corresponds to the number of tags associated
-with that particular final tag and the subsequent columns correspond to tags
+3. An array of the count of number of pixels in each tagged region
+4. An array of final tags corresponding to each tag
+5. Count of number of tags
+6. Count of number of final tags
 */
 
-void find_centroid(float sum_x[], float sum_y[], float weights[], int num_pixels[], int final_tag[], int arr_final_tags[][])
+void find_centroid(float sum_x[], float sum_y[], float weights[], int num_pixels[], int final_tag[], int num_tags, int num_final_tags)
 {
 
-//an array defined to contain the position of centroids of a star before writing on an external file
+//an array defined to contain the position of centroids of a star
 
-int arr_star_coordinates[50][2];    //[k][0]=pos_x  [k][1]=pos_y
+int arr_star_coordinates[40][2]=0;    //[k][0]=pos_x  [k][1]=pos_y
 
-//initialise variable to count number of stars
+//variable to count number of stars
 
 int num_stars=0;
 
-//initialise iteration variables
+//variable to count number of singly tagged stars
 
-int i,j;
+int num_single_tag_stars=0;
 
-//loop to find position of centroids of stars with single tagged region
+//variable to count number of zero rows in arr_sums
 
-for(i=0;final_tag[i]!=-1;i++)
+int num_zero_rows=0;
+
+//iteration variable
+
+int i=0;
+
+//array of variables to sum the values of different tagged regions of the same star ("summation variables")
+
+float tot_sum_x[num_final_tags]=0, tot_sum_y[num_final_tags]=0, tot_weights[num_final_tags]=0;
+int tot_pixels[num_final_tags]=0;
+
+//loop to find position of centroids of stars with single tagged region and update values of summation variables for multi tagged stars
+
+for(i=0; i<num_tags; i++)
     {
+    //is region singly tagged?
 
-    //is final_tag[i] 0 and number of pixels more than MINIMUM_PIXEL_COUNT?
+    if (final_tag[i] == 0)
 
-    if (num_pixels[i] < MINIMUM_PIXEL_COUNT || final_tag[i]!=0)
-        continue;
-
-    arr_star_coordinates[num_stars][0]=sum_x[i]/weights[i];
-    arr_star_coordinates[num_stars][1]=sum_y[i]/weights[i];
-
-    num_stars++;
-    }
-
-//variables to sum the values of different tagged regions of the same star
-
-float tot_sum_x=0,tot_sum_y=0,tot_weights=0;
-int tot_pixel_count=0;
-
-//loop to find position of centroids of stars with more than one tagged regions
-
-for(i=0;arr_final_tag[i][0]!=0;i++)
-    {
-    for(j=0;j<arr_final_tag[i][0];j++)
         {
-        tot_sum_x+=sum_x[arr_final_tag[i][j]];
-        tot_sum_y+=sum_y[arr_final_tag[i][j]];
-        tot_weights+=weights[arr_final_tag[i][j]];
-        tot_pixel_count+=num_pixels[arr_final_tag[i][j]];
-        }
+        //is number of pixels in region within range?
 
-    if (tot_pixel_count > MINIMUM_PIXEL_COUNT)
-        {
-        arr_star_coordinates[num_stars][0]=tot_sum_x/tot_weights;
-        arr_star_coordinates[num_stars][1]=tot_sum_y/tot_weights;
+        if (num_pixels[i] < MIN_PIXELS || num_pixels[i] > MAX_PIXELS)
+            continue;
 
+        arr_star_coordinates[num_final_tags + num_single_tag_stars][0] = sum_x[i]/weights[i];
+        arr_star_coordinates[num_final_tags + num_single_tag_stars][1] = sum_y[i]/weights[i];
+
+        num_single_tag_stars++;
         num_stars++;
         }
 
-    tot_sum_x=0;
-    tot_sum_y=0;
-    tot_weights=0;
-    tot_pixel_count=0;
+    else
+
+        {
+        //update summation variables
+
+        tot_sum_x[final_tag[i]]+=sum_x[i];
+        tot_sum_y[final_tag[i]]+=sum_y[i];
+        tot_weights[final_tag[i]]+=weights[i];
+        tot_pixels[final_tag[i]]+=num_pixels[i];
+        }
 
     }
 
+//loop to find position of centroids of stars with more than one tagged regions
+
+for(i=0; i<num_final_tags; i++)
+    {
+    //is number of pixels in region within range and with positive weight
+
+    if (tot_pixels[i] > MIN_PIXELS && tot_pixels[i] < MAX_PIXELS && tot_weights[i] > 0)
+        {
+        arr_star_coordinates[i - num_zero_rows][0] = tot_sum_x[i]/tot_weights[i];
+        arr_star_coordinates[i - num_zero_rows][1] = tot_sum_y[i]/tot_weights[i];
+
+        num_stars++;
+        }
+    else
+        num_zero_rows++;
+    }
+
+//to remove possible zero rows present between dense rows
+//is the number of zero rows between dense rows positive?
+
+if (num_zero_rows > 0)
+    {
+    for(i = (num_final_tags - num_zero_rows); i < num_stars; i++)
+        {
+        arr_star_coordinates[i][0] = arr_star_coordinates[i + num_zero_rows][0];
+        arr_star_coordinates[i][1] = arr_star_coordinates[i + num_zero_rows][1];
+        }
+    for(i = num_stars; i < (num_stars + num_zero_rows); i++)
+        {
+        arr_star_coordinates[i][0] = 0;
+        arr_star_coordinates[i][1] = 0;
+        }
+    }
+
 /*output consisting of
-1. centroids of stars (star_coordiantes)
+1. centroids of stars (arr_star_coordiantes)
 2. number of stars (num_stars)
 */
 
