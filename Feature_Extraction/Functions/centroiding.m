8c86484e4d1cf5e8617fc0860c4ca733f756581a
TheWeepingSage <kanabarmillen@gmail.com>
2020-05-26 15:39:04 +0530

    Change centroiding.m

--- Feature_Extraction/Functions/centroiding.m
+++ Feature_Extraction/Functions/centroiding.m
@@ -1,6 +1,5 @@
 function [arr_centroids] = centroiding(img_path)
-    arr_img = imread(img_path);
-    arr_img = rgb2gray(arr_img);
+    arr_img = rgb2gray(imread(img_path));
     [arr_sums_x, arr_sums_y, arr_weights, arr_num, arr_final_tags, tag_num, final_tag_num] = tag_2(arr_img);
     [arr_star_coordinates, num_stars] = merge_tag_3(arr_sums_x, arr_sums_y, arr_weights, arr_num, arr_final_tags, tag_num, final_tag_num);
     arr_centroids = zeros(num_stars, 3);
