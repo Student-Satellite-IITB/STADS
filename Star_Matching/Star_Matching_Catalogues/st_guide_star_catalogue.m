83590dbdd4d871cb196b582bc0709100a36948e5
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-05-28 18:17:59 +0530

    UPDATE - Star Catalogue
    UPDATE:
    st_Guide_Star_Catalogue & st_Preprocessed_Star_Catalogue moved to (Star_Matching\Star_Matching_Catalogues\Catalogues\) directory

---
+++ Star_Matching/Star_Matching_Catalogues/st_guide_star_catalogue.m
@@ -0,0 +1,19 @@
+%% Read SSP Star Catalogue
+SSP_SC = readtable('.\Catalogue\SKY2000\Catalogues\SSP_Star_Catalogue.csv');
+
+%% Create Guide Star Catalogue
+
+% Extract stars brighter than Limiting Magnitude
+Magnitude_Limit = 6;
+cond = SSP_SC.Vmag <= Magnitude_Limit; % Set up condition
+
+tmp = SSP_SC(cond , :);
+ 
+
+% Generate cartesian unit vectors
+unit_vect = rowfun(@ca_RA_DE_2_CartVect, tmp, 'InputVariables', [3,4], 'OutputVariableNames', {'X', 'Y', 'Z'});
+
+st_GD_SC = [tmp(:, 1), unit_vect]; % Append Columns
+%%
+% Write Table
+writetable(st_GD_SC, '.\Star_Matching\Star_Matching_Catalogues\Catalogues\st_Guide_Star_Catalogue.csv');
