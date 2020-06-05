29bca6eac0aa1a2dbd19c67cbb6745b84d862255
TheWeepingSage <kanabarmillen@gmail.com>
2020-06-05 13:18:27 +0530

    Update fe_centroiding_2.m
    updated the input format

--- Feature_Extraction/Functions/fe_centroiding_2.m
+++ Feature_Extraction/Functions/fe_centroiding_2.m
@@ -1,6 +1,6 @@
-function [arr_centroids] = fe_centroiding_2(img_path)
+function [arr_centroids] = fe_centroiding_2(arr_img)
     % reading in the image, converting it to grayscale
-    arr_img = rgb2gray(imread(img_path));
+    % arr_img = rgb2gray(imread(img_path));
     % getting interediate output from tag_2 and merge_tag_3
     [arr_sums_x, arr_sums_y, arr_weights, arr_num, arr_final_tags, tag_num, final_tag_num] = fe_tag_2(arr_img);
     [arr_star_coordinates, num_stars] = fe_merge_tag(arr_sums_x, arr_sums_y, arr_weights, arr_num, arr_final_tags, tag_num, final_tag_num);
