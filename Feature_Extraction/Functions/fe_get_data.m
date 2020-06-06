0d8baeefd2f21fb91e82a8dbb05a12ff8ec64045
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-06-06 20:06:08 +0530

    Update fe_get_data.m
    UPDATE - fe_get_data.m changed centroid_data from a table to a matrix (faster implementation). Also fixed the mix of x and y coordinates.

--- Feature_Extraction/Functions/fe_get_data.m
+++ Feature_Extraction/Functions/fe_get_data.m
@@ -2,19 +2,19 @@ function [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth, j_r
     %% loading constants
     load('fe_constants_region_growth.mat', 'THRESHOLD');
     %% base case
-    if arr_img(i_region_growth, j_region_growth) <= THRESHOLD
+    if arr_img(j_region_growth, i_region_growth) <= THRESHOLD
         return;
     end
-    
+
     %% updating centroid_data
     % -1 because of the padding added
-    centroid_data_st{star_num, 'x_sum'} = centroid_data_st{star_num, 'x_sum'} + arr_img(i_region_growth, j_region_growth) * (i_region_growth - 1);
-    centroid_data_st{star_num, 'y_sum'} = centroid_data_st{star_num, 'y_sum'} + arr_img(i_region_growth, j_region_growth) * (j_region_growth - 1);
-    centroid_data_st{star_num, 'pixel_sum'} = centroid_data_st{star_num, 'pixel_sum'} + arr_img(i_region_growth, j_region_growth);
-    centroid_data_st{star_num, 'num_pixels'} = centroid_data_st{star_num, 'num_pixels'} + 1;
+    centroid_data_st(star_num, 1) = centroid_data_st(star_num, 1) + arr_img(j_region_growth, i_region_growth) * (i_region_growth - 1);
+    centroid_data_st(star_num, 2) = centroid_data_st(star_num, 2) + arr_img(j_region_growth, i_region_growth) * (j_region_growth - 1);
+    centroid_data_st(star_num, 3) = centroid_data_st(star_num, 3) + arr_img(j_region_growth, i_region_growth);
+    centroid_data_st(star_num, 4) = centroid_data_st(star_num, 4) + 1;
     % setting to 0 so that i does not go into infinite loop
-    arr_img(i_region_growth, j_region_growth) = 0;
-    
+    arr_img(j_region_growth, i_region_growth) = 0;
+
     %% recursive call to 4 neighbours
     [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth - 1, j_region_growth, star_num, centroid_data_st);
     [arr_img, centroid_data_st] = fe_get_data(arr_img, i_region_growth + 1, j_region_growth, star_num, centroid_data_st);
