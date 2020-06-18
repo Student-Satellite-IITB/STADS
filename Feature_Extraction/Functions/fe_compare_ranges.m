b14eca5802a5698e9fbbcd39cc26105a48e40290
TheWeepingSage <kanabarmillen@gmail.com>
2020-06-18 15:11:16 +0530

    Change fe_compare_ranges.m Updated the code to a simpler implementation

--- Feature_Extraction/Functions/fe_compare_ranges.m
+++ Feature_Extraction/Functions/fe_compare_ranges.m
@@ -1,8 +1,5 @@
 function [bool_comp] = fe_compare_ranges(start_1, end_1, start_2, end_2)
 % returns true if the two ranges intersect at some pixel, returns false
 % otherwise
-bool_comp = false;
-if (start_1<=start_2 && start_2<=end_1) || (start_1 <= end_2 && end_2 <= end_1) ||  (start_2<=start_1 && end_1<=end_2)
-    bool_comp = true;
-end
+    bool_comp = (start_1<=start_2 && start_2<=end_1) || (start_1 <= end_2 && end_2 <= end_1) ||  (start_2<=start_1 && end_1<=end_2);
 end
