b70a0c96a601fe7f7b998602ca940a1566bbe9f8
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-06-21 21:06:44 +0530

    UPDATE - Star Matching | Fix Body-frame error
    UPDATE:
    ---------
    1. st_gnrt_bi.m
    Fix body-frame error, which was preventing Estimation block from determining the attitude
    
    2. st_MILS_plot.m -
    Plot star-matching results without verification step

--- Star_Matching/4_Star_Matching/MILS/st_MILS_plot.m
+++ Star_Matching/4_Star_Matching/MILS/st_MILS_plot.m
@@ -16,7 +16,7 @@ load(file_loc_ip);
 T = num2str(T_HOLD);
 %% Plot
 X = 1: height(data_table);
-
+figure();
 N_True = data_table.st_N_True;
 N_False = data_table.st_N_False;
 N_Miss = data_table.st_N_Miss;
@@ -30,11 +30,37 @@ plot(X, N_str, '-sk', 'LineWidth', 1.5);
 yline(6, '--k');
 hold off
 xlim([X(1) X(end)])
-legend('True', 'False', 'Miss', 'Image', '\delta_{TM}:6', 'Location', 'north');
+legend('True', 'False', 'Miss', 'Image', 'N_{TH}:6', 'Location', 'north');
 xlabel('Image Number');
 xticks(X);
 ylabel('Number of such instances $\rightarrow$');
 title(strcat('ST - Summary | Threshold value: ', T));
 
 print(strcat(file_base_op, 'threshold_', T), '-dpng', '-r500'); 
+
+%% Without Verification step
+% Enter Threshold value
+T = num2str(T_HOLD);
+% Plot
+X = 1: height(data_table);
+
+N_True = data_table.st_N_True + data_table.st_N_Miss;
+N_False = data_table.st_N_Match-N_True;
+N_str = data_table.fe_n_str;
+
+figure();
+hold on
+plot(X, N_True, '-sg', 'LineWidth', 1.5);
+plot(X, N_False, '-sr', 'LineWidth', 1.5);
+plot(X, N_str, '-sk', 'LineWidth', 1.5);
+yline(6, '--k');
+hold off
+xlim([X(1) X(end)])
+legend('True', 'False', 'Image', 'N_{TH}:6', 'Location', 'north');
+xlabel('Image Number');
+xticks(X);
+ylabel('Number of such instances $\rightarrow$');
+title(strcat('ST - Summary | No Verifiaction | Threshold value: ', T));
+
+print(strcat(file_base_op, 'no_ver_threshold_', T), '-dpng', '-r500'); 
 disp('Image Generation: Done');
