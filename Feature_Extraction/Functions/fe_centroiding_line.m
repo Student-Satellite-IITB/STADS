74729552d4a61357cf773d24c7e23a4fdcc121a0
TheWeepingSage <kanabarmillen@gmail.com>
2020-06-29 13:05:41 +0530

    Change fe_centroiding_line.m Updated code, still incomplete

--- Feature_Extraction/Functions/fe_centroiding_line.m
+++ Feature_Extraction/Functions/fe_centroiding_line.m
@@ -9,9 +9,20 @@ output:
         centroid coordinates the second and third columns
 %}
 
-    load("constants_feature_extraction_3.mat", "LENGTH", "NUM_REGIONS");
+    load("constants_feature_extraction_3.mat", "LENGTH", "NUM_REGIONS", "NUM_RANGES_ROW", "BREADTH");
     arr_centroids = zeros(NUM_REGIONS, 3);
     num_stars = 0;
+    arr_centroid_data = zeros(NUM_REGIONS, 5);
     
+    [arr_row, num_ranges] = fe_extract_row_data(arr_image(1, :));
+    arr_centroid_data(1:num_ranges, 1) = arr_row(1:num_ranges, 1);
+    arr_centroid_data(1:num_ranges, 3:4) = arr_row(1:num_ranges, 2:3);
+    arr_centroid_data(1:num_ranges, 2) = arr_row(1:num_ranges, 2);
+    num_tags = num_ranges;
+    arr_merge_regions = 0;
+    num_merge_regions = 0;
     
+    for i_row = 2:BREADTH
+        
+    end
 end
