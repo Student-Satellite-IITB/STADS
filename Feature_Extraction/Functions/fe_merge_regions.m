a9f2c62de99c57f28a776277be5c9b96f9ac7437
TheWeepingSage <kanabarmillen@gmail.com>
2020-06-29 13:02:54 +0530

    Change fe_merge_regions.m Added explanation for input and output and fixed an error

--- Feature_Extraction/Functions/fe_merge_regions.m
+++ Feature_Extraction/Functions/fe_merge_regions.m
@@ -1,8 +1,21 @@
 function arr_centroids = fe_merge_regions(arr_centroids, arr_merge_regions, num_merge_regions)
-    if num_merge_regions>0
-        for i_merge = 1:num_merge_regions
+%{
+input:
+-arr_centroids:
+    array cnontaining data corresponding to tagged regions
+-arr_merge_regions:
+    array containing the regions to be merged
+-num_merge_regions:
+    number of rows containing merge data in arr_merge_regions
+%}
+    load("constants_feature_extraction_3.mat", "NUM_MERGE_LINE")
+    for i_merge = 1:NUM_MERGE_LINE
+        if num_merge_regions == 0
+            break
+        else
             if arr_merge_regions(i_merge, 1) > 0
                 merge_row_to = arr_merge_regions(i_merge, 2);
+                num_merge_regions = num_merge_regions-1;
                 for i_merge_row = 3:arr_merge_regions(i_merge, 1) + 1
                     arr_centroids(merge_row_to, :) = arr_centroids(merge_row_to, :) + arr_centroids(arr_merge_regions(i_merge, i_merge_row), :);
                 end
